a097bb8289a28fea4d977a464c092c0181fcd4fc
NIFFYNATION <adewaoluwanifemi@gmail.com>
2025-12-09 16:53:07 +0100

    nnb

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,27 @@
-import {  useEffect } from 'react';
-import { Outlet } from 'react-router-dom'
+import { useEffect } from 'react';
+import { Outlet, ScrollRestoration } from "react-router-dom";
+
 
 
 
 
 function App() {
-  
-  return (
-    
-    <div className="min-h-screen">
-            {/* Fixed position theme toggle */}
-            <div className="fixed bottom-20 right-4 z-50">
-              {/* dd */}
-            </div>
 
-            <Outlet />
-            {/* Mobile Tab Navigation */}
-            <div className="md:hidden pt-32">      
-            </div>
-          </div>
+  return (
+
+    <div className="min-h-screen">
+      {/* Fixed position theme toggle */}
+      <div className="fixed bottom-20 right-4 z-50">
+        {/* dd */}
+      </div>
+
+      <Outlet />
+      <ScrollRestoration />
+
+      {/* Mobile Tab Navigation */}
+      <div className="md:hidden pt-32">
+      </div>
+    </div>
   )
 }
 export default App
